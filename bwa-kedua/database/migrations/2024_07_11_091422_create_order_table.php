3dddfa5c91ad738760a4ee0705a24a9855463823
paturr123 <khafialalf@gmail.com>
2024-07-11 18:07:31 +0800

    menerapkan relation dan foreign keys

---
+++ bwa-kedua/database/migrations/2024_07_11_091422_create_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buyer_id')->nullable()->index('fk_order_buyer_to_users');
+            $table->foreignId('freelancer_id')->nullable()->index('fk_order_freelancer_to_users');
+            $table->foreignId('service_id')->nullable()->index('fk_order_to_service');
+            $table->longText('file')->nullable();
+            $table->longText('note')->nullable();
+            $table->date('expired')->nullable();
+            $table->foreignId('order_status_id')->nullable()->index('fk_order_to_order_status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
