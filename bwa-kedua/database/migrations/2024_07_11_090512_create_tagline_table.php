2326aea857188b4077dae536f93e8a0e85979a07
paturr123 <khafialalf@gmail.com>
2024-07-11 21:53:13 +0800

    menambahkan relation dan foreign keys

---
+++ bwa-kedua/database/migrations/2024_07_11_090512_create_tagline_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tagline', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->nullable()->index('fk_tagline_to_service');
+            $table->string('tagline')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tagline');
+    }
+};
---
+++ bwa-kedua/database/migrations/2024_07_11_090536_create_thumbnail_service_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('thumbnail_service', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->nullable()->index('fk_thumbnail_service_to_service');
+            $table->longText('thumbnail')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('thumbnail_service');
+    }
+};
